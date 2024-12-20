10709389c90d78f3359a6a7ce1b0a37c51f06015
Dan Rosenberg Osted <danosted@gmail.com>
2024-12-20 10:46:32 +0100

    untrack

--- 2024/aoc2024/FilerLoaderService.cs
+++
@@ -1,9 +0,0 @@
-public static class FilerLoaderService
-{
-    public static IEnumerable<string> GetInputFile(AocTask task)
-    {
-        var inputLists = File.ReadAllLines($"Assets\\{task.Filename}");
-
-        return inputLists;
-    }
-}
