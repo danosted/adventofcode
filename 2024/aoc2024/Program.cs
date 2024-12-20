384f80cfadef8323fb498c3635f5fc0b8ecce87d
Dan Rosenberg Osted <danosted@gmail.com>
2024-12-20 10:44:17 +0100

    initial

---
+++ 2024/aoc2024/Program.cs
@@ -0,0 +1,21 @@
+﻿var runNumber = args.FirstOrDefault() ?? "1";
+
+var isTest = args.Count() == 2 && args[1] == "true";
+// #if DEBUG
+// Console.WriteLine("DEBUG");
+// isTest = true;
+// runNumber = "2";
+// #endif
+if (runNumber == "1")
+{
+    var service = Activator.CreateInstance<PartOneService>();
+    service.Run(isTest);
+}
+
+if (runNumber == "2")
+{
+    var service = Activator.CreateInstance<PartTwoService>();
+    service.Run(isTest);
+}
+
+
